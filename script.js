b860106512ff05e42dacfb0503e605c29059fea2
MajorJann <litvinovajann@gmail.com>
2021-01-27 22:46:41 +0300

    changed script.js

--- script.js
+++ script.js
@@ -1,35 +1,54 @@
-document.getElementsByClassName('.open-overlay').onclick(function() {
-    var overlay_navigation = $('.overlay-navigation'),
-      nav_item_1 = $('nav li:nth-of-type(1)'),
-      nav_item_2 = $('nav li:nth-of-type(2)'),
-      nav_item_3 = $('nav li:nth-of-type(3)'),
-      nav_item_4 = $('nav li:nth-of-type(4)'),
-      nav_item_5 = $('nav li:nth-of-type(5)'),
-      top_bar = $('.bar-top'),
-      middle_bar = $('.bar-middle'),
-      bottom_bar = $('.bar-bottom');
-  
-    overlay_navigation.toggleClass('overlay-active');
-    if (overlay_navigation.hasClass('overlay-active')) {
-  
-      top_bar.removeClass('animate-out-top-bar').addClass('animate-top-bar');
-      middle_bar.removeClass('animate-out-middle-bar').addClass('animate-middle-bar');
-      bottom_bar.removeClass('animate-out-bottom-bar').addClass('animate-bottom-bar');
-      overlay_navigation.removeClass('overlay-slide-up').addClass('overlay-slide-down')
-      nav_item_1.removeClass('slide-in-nav-item-reverses').addClass('slide-in-nav-item');
-      nav_item_2.removeClass('slide-in-nav-item-delay-1-reverse').addClass('slide-in-nav-item-delay-1');
-      nav_item_3.removeClass('slide-in-nav-item-delay-2-reverse').addClass('slide-in-nav-item-delay-2');
-      nav_item_4.removeClass('slide-in-nav-item-delay-3-reverse').addClass('slide-in-nav-item-delay-3');
-      nav_item_5.removeClass('slide-in-nav-item-delay-4-reverse').addClass('slide-in-nav-item-delay-4');
-    } else {
-      top_bar.removeClass('animate-top-bar').addClass('animate-out-top-bar');
-      middle_bar.removeClass('animate-middle-bar').addClass('animate-out-middle-bar');
-      bottom_bar.removeClass('animate-bottom-bar').addClass('animate-out-bottom-bar');
-      overlay_navigation.removeClass('overlay-slide-down').addClass('overlay-slide-up')
-      nav_item_1.removeClass('slide-in-nav-item').addClass('slide-in-nav-item-reverse');
-      nav_item_2.removeClass('slide-in-nav-item-delay-1').addClass('slide-in-nav-item-delay-1-reverse');
-      nav_item_3.removeClass('slide-in-nav-item-delay-2').addClass('slide-in-nav-item-delay-2-reverse');
-      nav_item_4.removeClass('slide-in-nav-item-delay-3').addClass('slide-in-nav-item-delay-3-reverse');
-      nav_item_5.removeClass('slide-in-nav-item-delay-4').addClass('slide-in-nav-item-delay-4-reverse');
+document.querySelector('.open-overlay').addEventListener("click",
+    function openMenu() {
+        let overlay_navigation = document.querySelector('.overlay-navigation');
+        let nav_item_1 = document.querySelector('nav li:nth-of-type(1)');
+        let nav_item_2 = document.querySelector('nav li:nth-of-type(2)');
+        let nav_item_3 = document.querySelector('nav li:nth-of-type(3)');
+        let nav_item_4 = document.querySelector('nav li:nth-of-type(4)');
+        let nav_item_5 = document.querySelector('nav li:nth-of-type(5)');
+        let top_bar = document.querySelector('.bar-top');
+        let middle_bar = document.querySelector('.bar-middle');
+        let bottom_bar = document.querySelector('.bar-bottom');
+        overlay_navigation.classList.toggle('overlay-active');
+
+        if (overlay_navigation.classList.contains('overlay-active')) {
+            top_bar.classList.remove('animate-out-top-bar');
+            top_bar.classList.add('animate-top-bar');
+            middle_bar.classList.remove('animate-out-middle-bar');
+            middle_bar.classList.add('animate-middle-bar');
+            bottom_bar.classList.remove('animate-out-bottom-bar');
+            bottom_bar.classList.add('animate-bottom-bar');
+            overlay_navigation.classList.remove('overlay-slide-up');
+            overlay_navigation.classList.add('overlay-slide-down');
+            nav_item_1.classList.remove('slide-in-nav-item-reverses');
+            nav_item_1.classList.add('slide-in-nav-item');
+            nav_item_2.classList.remove('slide-in-nav-item-delay-1-reverse');
+            nav_item_2.classList.add('slide-in-nav-item-delay-1');
+            nav_item_3.classList.remove('slide-in-nav-item-delay-2-reverse');
+            nav_item_3.classList.add('slide-in-nav-item-delay-2');
+            nav_item_4.classList.remove('slide-in-nav-item-delay-3-reverse');
+            nav_item_4.classList.add('slide-in-nav-item-delay-3');
+            nav_item_5.classList.remove('slide-in-nav-item-delay-4-reverse');
+            nav_item_5.classList.add('slide-in-nav-item-delay-4');
+        } else {
+            top_bar.classList.remove('animate-top-bar');
+            top_bar.classList.add('animate-out-top-bar');
+            middle_bar.classList.remove('animate-middle-bar');
+            middle_bar.classList.add('animate-out-middle-bar');
+            bottom_bar.classList.remove('animate-bottom-bar');
+            bottom_bar.classList.add('animate-out-bottom-bar');
+            overlay_navigation.classList.remove('overlay-slide-down');
+            overlay_navigation.classList.add('overlay-slide-up')
+            nav_item_1.classList.remove('slide-in-nav-item');
+            nav_item_1.classList.add('slide-in-nav-item-reverse');
+            nav_item_2.classList.remove('slide-in-nav-item-delay-1');
+            nav_item_2.classList.add('slide-in-nav-item-delay-1-reverse');
+            nav_item_3.classList.remove('slide-in-nav-item-delay-2');
+            nav_item_3.classList.add('slide-in-nav-item-delay-2-reverse');
+            nav_item_4.classList.remove('slide-in-nav-item-delay-3');
+            nav_item_4.classList.add('slide-in-nav-item-delay-3-reverse');
+            nav_item_5.classList.remove('slide-in-nav-item-delay-4');
+            nav_item_5.classList.add('slide-in-nav-item-delay-4-reverse');
+        }
     }
-  })
+)
